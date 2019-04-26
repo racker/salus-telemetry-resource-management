2068620aecaaabb76f921c731eabaef4a593ac37
Adam <harageth@gmail.com>
2019-04-25 17:06:08 -0700

    got the conversion to work correctly

--- src/main/java/com/rackspace/salus/resource_management/swaggerJsonConverter.java
+++ src/main/java/com/rackspace/salus/resource_management/swaggerJsonConverter.java
@@ -4,10 +4,9 @@ package com.rackspace.salus.resource_management;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.commons.lang3.StringUtils;
-import org.codehaus.jackson.node.ArrayNode;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
@@ -20,22 +19,34 @@ public class swaggerJsonConverter {
         ObjectMapper mapper = new ObjectMapper();
         String content = new Scanner(new File("/Users/adam9500/workspace/salus-telemetry-bundle/apps/resource-management/target/generated/swagger/swagger.json")).useDelimiter("\\Z").next();
         ObjectNode root = (ObjectNode)mapper.readTree(content);
-
+        Map<String, JsonNode> temp = new HashMap();
         for (Iterator<Map.Entry<String, JsonNode>> it = root.get("paths").fields(); it.hasNext(); ) {
             Map.Entry<String, JsonNode> elt = it.next();
             if (elt.getKey().contains("tenant"))
             {
-                ((ObjectNode)root.get("paths")).set(
+                String newKey = elt.getKey().replace("tenant/{tenantId}/", "");
+                temp.put(newKey, elt.getValue());
+
+               /* ((ObjectNode)root.get("paths")).set(
                         elt.getKey().replace("tenant/{tenantId}/", ""),
                         elt.getValue());
+                */
+                //another foreach
+                //elt.getValue().fields();
 
-                ((ObjectNode) root.get("paths")).remove(elt.getKey());
+                it.remove();
+                //((ObjectNode) root.get("paths")).remove(elt.getKey());
             } else {
-                ((ObjectNode) root.get("paths")).remove(elt.getKey());
+                it.remove();
+                //((ObjectNode) root.get("paths")).remove(elt.getKey());
             }
         }
 
-        mapper.writeValue(new java.io.File("convertedOutput.json"), (JsonNode)root);
+        temp.forEach((key, node)->{
+            root.put(key, node);
+        });
+
+        mapper.writeValue(new java.io.File("/Users/adam9500/workspace/salus-telemetry-bundle/apps/resource-management/target/generated/swagger/convertedOutput.json"), (JsonNode)root);
 
     }
 }
